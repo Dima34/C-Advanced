3e643b7f4f9d0040bd52b35f518aa24fb8b95b40
Dima <vavilov9823@gmail.com>
2023-04-07 07:02:38 +0300

    Lambda, delegates and linq added

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿// Extension methods exists for adding methods to an existing class without changing its source code or creating a new class
+
+string post = "This is supposed to be a very long post...";
+string shorten = post.Shorten(5);
+
+
+Console.ReadLine();
+
+class Class : EventArgs
+{
+    
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿List<Book> bookList = new List<Book>()
+{
+    new Book(){Title = "Lev tolstoy", Price = 10},
+    new Book(){Title = "Vinny Puh", Price = 15},
+    new Book(){Title = "Iskustvo pofigizma", Price = 20},
+    new Book(){Title = "Karl Marks", Price = 7},
+    new Book(){Title = "Bible", Price = 200}
+};
+
+var cheaperBooks = from b in bookList
+    where b.Price < 10
+    orderby b.Title
+    select b;
+// The same as 
+var cheapBooks = 
+    bookList.Where(b => b.Price < 10)
+    .OrderBy(b => b.Title)
+    .Select(b => b);
+    
+var last2 = bookList.Skip(2).Take(3);
+Console.WriteLine(last2);
+
+DateTime date = null;
+
+Console.ReadLine();
