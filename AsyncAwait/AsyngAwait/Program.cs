506d9ca6bab263fadef2d4b428e1d5db16831ce4
Dima34 <57358582+Dima34@users.noreply.github.com>
2023-04-13 00:03:49 +0300

    Async examples added

--- AsyncAwait/AsyngAwait/Program.cs
+++ AsyncAwait/AsyngAwait/Program.cs
@@ -1,22 +1,43 @@
-﻿var contentLoader = new ContentLoader();
+﻿using System.Collections;
+
+var contentLoader = new ContentLoader();
 await contentLoader.DownloadMultipleTaskAsync("https://learn.microsoft.com/ru-ru/dotnet/csharp/");
 Console.WriteLine("Another jobs in main thread...");
 
-await contentLoader.WaitForAsync("Панаc");
-await contentLoader.WaitForAsync("Yurko");
-await contentLoader.WaitForAsync("Ignat");
+// await contentLoader.WaitForAsync("Панаc");
+// await contentLoader.WaitForAsync("Yurko");
+// await contentLoader.WaitForAsync("Ignat");
+//
+// var forDima = contentLoader.WaitForAsync("Dima");
+// var forKirill = contentLoader.WaitForAsync("Kirill");
+// var forMaksym = contentLoader.WaitForAsync("Maksym");
+//
+// await forDima;
+// await forKirill;
+// await forMaksym;
 
-var forDima = contentLoader.WaitForAsync("Dima");
-var forKirill = contentLoader.WaitForAsync("Kirill");
-var forMaksym = contentLoader.WaitForAsync("Maksym");
+// contentLoader.delayedPrinter("hello world");
+// contentLoader.delayedPrinter("What`s the weather?");
 
-await forDima;
-await forKirill;
-await forMaksym;
+Action SomeEvent = EventReaction;
+SomeEvent?.Invoke();
 
-contentLoader.delayedPrinter("hello world");
-contentLoader.delayedPrinter("What`s the weather?");
+async void EventReaction()
+{
+    await Task.Delay(3000);
+    Console.WriteLine("Wooow sheesh! That event done 3 minutes ago!");
+}
 
+
+async Task<int> CalculateTheHardestExpressionEver()
+{
+    Console.WriteLine("Calculating hardest expression ever...");
+    Task.Delay(5000);
+    return 2 + 2;
+}
+
+var HardestEverResult = CalculateTheHardestExpressionEver();
+Console.WriteLine($"The hardest result is {HardestEverResult.Result}");
 
 Console.WriteLine("\n Program done");
 Console.ReadLine();
